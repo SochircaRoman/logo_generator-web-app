f6841d762254ec4f05a2ddba35bb87f874d74818
User01 <romansochirca30@gmail.com>
2023-02-19 12:27:05 +0200

    server.js configuration

---
+++ server/src/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+      app.listen(process.env.SERVER_PORT, process.env.SERVER_URL, () => {
+        console.log(`Server started on PORT = ${process.env.SERVER_PORT}`)
+      })
+  } catch (error) {
+      console.log(error)
+  }
+};
+
+startServer();
