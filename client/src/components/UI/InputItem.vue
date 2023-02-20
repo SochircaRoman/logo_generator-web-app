0dfa83e35e1f7398ac91647a4679c53ffa210679
User01 <romansochirca30@gmail.com>
2023-02-20 12:39:59 +0200

    Create UI InputItem

---
+++ client/src/components/UI/InputItem.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <div class="input_group">
+    <label :for="name"><b>{{ name.charAt(0).toUpperCase() + name.slice(1) }}</b></label>
+    <Field :name="name" :type="type" :placeholder="placeholder" class="input_field" :validateOnInput="true" />
+    <ErrorMessage :name="name" class="error-feedback" />
+  </div>
+  
+</template>
+
+<script>
+import { Field, ErrorMessage } from "vee-validate";
+
+export default {
+  props: ["type", "name", "placeholder"],
+  components: {
+    Field,
+    ErrorMessage,
+  },
+}
+</script>
+
+<style>
+
+.input_group {
+  margin-bottom: 15px;
+}
+
+.input_field {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+.error-feedback {
+  color: #f23648;
+}
+
+</style>
