abcf4f05dec652af49f42b60b26f608f447378f7
User01 <romansochirca30@gmail.com>
2023-02-20 12:41:16 +0200

    Create SuccesItem

---
+++ client/src/components/UI/SuccesItem.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div class="container">
+    <div class="top">
+      <div class="top__content">
+        <img src="png/succes.png" alt="SUCCES">
+        <h3 v-if="text" class="top__text"> {{ text }}</h3>
+        <h3 v-else class="top__text">Succes!</h3>
+      </div>
+    </div>
+    <div class="bottom">
+      <p class="bottom__text">Please wait while redirecting...</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  props: ["text"]
+}
+</script>
+
+<style>
+.container {
+  width: 300px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px gray;
+  text-align: center;
+}
+
+.top {
+  background-color: #333;
+  color: white;
+  border-radius: 10px 10px 0px 0px;
+  padding: 25px;
+}
+
+.top__text {
+  font-size: 30px;
+  font-weight: 600;
+  margin-top: 20px;
+}
+
+.bottom__text {
+  font-size: 20px;
+  font-weight: 400;
+  padding: 30px 0px;
+  color:#333;
+}
+
+</style>
