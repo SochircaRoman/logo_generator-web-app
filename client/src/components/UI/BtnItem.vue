127a73aca8bfe50e722ab36f78531057685ab591
User01 <romansochirca30@gmail.com>
2023-02-20 12:38:10 +0200

    Create UI BtnItem

---
+++ client/src/components/UI/BtnItem.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div class="btn__item">
+    <button class="submit_btn">
+      <span>{{ btnName }}</span>
+    </button>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: ["btnName"]
+}
+</script>
+
+<style>
+
+.submit_btn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  font-size: 20px;
+}
+
+.submit_btn:hover {
+  opacity: 0.8;
+}
+
+</style>
