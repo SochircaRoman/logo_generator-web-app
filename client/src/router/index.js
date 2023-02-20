d76cbf40196a33fb0dd30226fd5fd2ece4679d32
User01 <romansochirca30@gmail.com>
2023-02-20 12:29:03 +0200

    Add index.js to router

---
+++ client/src/router/index.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
+    },
+  ]
+})
+
+export default router
