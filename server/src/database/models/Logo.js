469cb1af709566b77859d93b3e2a9cec66660ecc
User01 <romansochirca30@gmail.com>
2023-05-14 20:09:40 +0300

    Define Logo model

---
+++ server/src/database/models/Logo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const LogoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+  userId: [
+    { type: Schema.Types.ObjectId, ref: 'User' }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model("Logo", LogoSchema)
